e232de7e08de3ad048772776b87c1517bc26c92b
Hélio Júnior <heliomajr@hotmail.com>
2019-02-20 01:10:36 -0300

    Acrescenta o arquivo de variáveis globais do projeto

---
+++ GlobalVar.py
@@ -0,0 +1,6 @@
+#GLOBAL VARIABLES 
+
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = ("two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace")
+values = ("two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"jack":10,"queen":10,"king":10,"ace":11)
+playing = True
