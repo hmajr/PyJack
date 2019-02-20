e750aa485953391d3aa306c9f8c3e7b3290a7786
Hélio Júnior <heliomajr@hotmail.com>
2019-02-20 01:19:12 -0300

    Testa variáveis criadas

--- GlobalVar.py
+++ GlobalVar.py
@@ -4,3 +4,7 @@ suits = ("Hearts", "Diamonds", "Spades", "Clubs")
 ranks = ("two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace")
 values = ("two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"jack":10,"queen":10,"king":10,"ace":11)
 playing = True
+
+print(suits)
+print(ranks)
+print(values)
