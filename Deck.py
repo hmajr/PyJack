ad867483d79e8e74df4c9a34f5c834ed42c0ea39
Hélio Júnior <heliomajr@hotmail.com>
2019-02-28 12:27:09 -0300

    Atualiza nomes das variaveis globais. Adciona classe Deck.py

---
+++ Deck.py
@@ -0,0 +1,22 @@
+import Card
+from GlobalVar import SUITS
+from GlobalVar import RANKS
+
+class Deck():
+	"""Baralho de cartas"""
+	#ATTRIBUTES
+	cards = ("","")
+	#METHODS
+	def __init__(self):
+		for suit in SUITS:
+			for rank in RANKS:
+				cards += (x,y)
+
+	def RetriveCard():
+		pass
+
+	def Shuffle():
+		pass
+
+	def IsEmpty():
+		pass
--- GlobalVar.py
+++ GlobalVar.py
@@ -1,10 +1,6 @@
 #GLOBAL VARIABLES 
 
-suits = ("Hearts", "Diamonds", "Spades", "Clubs")
-ranks = ("two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace")
-values = ("two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"jack":10,"queen":10,"king":10,"ace":11)
+SUITS = ("Hearts", "Diamonds", "Spades", "Clubs")
+RANKS = ("two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace")
+VALUES = {"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"jack":10,"queen":10,"king":10,"ace":11}
 playing = True
-
-print(suits)
-print(ranks)
-print(values)
