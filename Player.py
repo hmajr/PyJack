33cb8f8f0d8157823bc8ee46e6c402b4cec22850
Hélio Júnior <heliomajr@hotmail.com>
2019-02-28 12:40:36 -0300

    Adciona classe Player.py.

---
+++ Player.py
@@ -0,0 +1,19 @@
+class Player():
+	"""Classe do jogador"""
+	#ATTRIBUTES
+	nome = ""
+	chips = 0
+	hand = []
+
+
+	#METHODS
+	def __init__(self, nome, chips):
+		self.nome = nome
+
+	def TakeOneCard():
+		pass
+
+	def FazerAposta():
+		pass
+
+	
