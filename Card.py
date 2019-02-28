ea6ed88b1151e713130d282f3782105b40ca056d
Hélio Andrade <hmajr.tec@gmail.com>
2019-02-28 18:29:34 -0300

    Adciona método print_card(). Printa gráfico carta.

--- Card.py
+++ Card.py
@@ -9,10 +9,13 @@ class Card():
 	#Methods
 	def __init__(self):
 		pass
-		
-	def __init__(self, suit, rank):
+
+	def __init__(self, suit = None, rank = None):
 		self.suit = suit
 		self.rank = rank
 
 	def __str__(self):
 		return f"Suit: {self.suit}, Rank: {self.rank}"
+
+	def print_card():
+		pass
