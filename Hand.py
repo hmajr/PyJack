ae8d92afeefa80fc99a0a22fc7a758a74f9c83a4
Hélio Andrade <hmajr.tec@gmail.com>
2019-02-28 23:58:36 -0300

    Cria DOCSTRING da classe Hand. Corrige erro no método add_cart().

--- Hand.py
+++ Hand.py
@@ -1,15 +1,32 @@
+"""Arquivo classe Hand
+"""
 from Card import Card
 from Deck import Deck
 
 class Hand():
-	"""Representa a mão do jogador"""
+	"""Representa a mão do jogador
+	
+	Attributes:
+	    ace (int): flag para controle do valor do ÁS
+	    cards (list): cartas da mão de jogo
+	    value (int): pontuação da mão atual
+	"""
 	def __init__(self):
+		"""Inicializa mão com valores padrões
+		"""
 		self.cards = []
 		self.value = 0
 		self.ace = 0
 
-	def add_card(self, deck):
-		self.cards.append(deck.retrive_card())
+	def add_card(self, card):
+		"""Adciona uma carta a mão atual
+		
+		Args:
+		    deck (Card Class): carta a adcionar
+		"""
+		self.cards.append(card)
 
 	def adjust_for_ace(self):
+		"""Ajusta valor do ÁS quando necessário
+		"""
 		pass
