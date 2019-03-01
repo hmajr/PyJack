55c82d0062fd8e6ad21128c4d8cd8eed3711732f
Hélio Andrade <hmajr.tec@gmail.com>
2019-02-28 22:34:11 -0300

    Adciona classe Chips. Gerencia quantidade de fichas e métodos de entrada e saída de fichas

---
+++ Chips.py
@@ -0,0 +1,13 @@
+from GlobalVar import CHIPS_DEFAULT
+
+class Chips():
+	"""Classe de fichas"""
+	def __init__(self):
+		self.total = CHIPS_DEFAULT
+		self.bet = 0
+
+	def win_bet():
+		pass
+		
+	def lose_bet():
+		pass
