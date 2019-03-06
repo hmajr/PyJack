d548842fe996062b9aef84bcc8f967862107a875
Hélio Andrade <hmajr.tec@gmail.com>
2019-03-06 00:22:46 -0300

    Implementa adjuste_for_ace(), delete_hand()

--- Hand.py
+++ Hand.py
@@ -2,7 +2,7 @@
 """
 from Card import Card
 from Deck import Deck
-
+from GlobalVar import VALUES
 class Hand():
 	"""Representa a mão do jogador
 	
@@ -25,19 +25,48 @@ class Hand():
 		    deck (Card Class): carta a adcionar
 		"""
 		self.cards.append(card)
+		self.value += VALUES[card.rank]
+		if card.rank == "ace":
+			self.ace += 1
+
+		self.adjust_for_ace()
+
 
 	def adjust_for_ace(self):
 		"""Ajusta valor do ÁS quando necessário
 		"""
-		pass
+		if self.value > 21 and self.ace > 0:
+			for num_times in range(0, self.ace):
+				self.value -= 10
+				self.ace -= 1
+				if self.value < 21:
+					break
 
-	def print_hand(num_cards = None):
+
+	def print_hand(self, num_cards = None):
+
+
 		if num_cards == None:
-			num_cards = len(self.card)
+			num_cards = len(self.cards)
 
-		for num_line in range(0,len(self.card.graphCard)):
-			for num_cards in range(0,num_cards):
-				print(self.card[num_card].graphCard[num_line])
+		for card_index in range(0, num_cards):
+			for line in range(0, len(self.cards[0].graphCard)):
+				print(self.cards[card_index].graphCard[line])
+
+	#	# Implementa print de cartas horizontalmente
+	#   # ISSUE: Não lida com caso esconder carta dealer,
+	# 	# 		 origem problema: linha append, não iterativo
+		# print_list = []
+		# for card_index in range(0, num_cards):
+		# 	for line in range(0, len(self.cards[0].graphCard)):
+		# 		print_list.append(self.cards[card_index].graphCard[line] + " " + self.cards[card_index+1].graphCard[line])
+		# for line in range(0, len(print_list)):
+		# 	print(print_list[line])
 
 	def hand_points(self):
 		return self.value
+
+	def delete_hand(self):
+		"""Deleta deck atual
+		"""
+		del(self.cards[:])
